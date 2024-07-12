b9f6b418844ef2642d04375c15928f4664857a5c
syam1310 <psyam0175@gmail.com>
2024-07-12 21:10:26 +0530

    building toDoList app using Flutter

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(home: MyHomePage(),));  
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() =>  _MyHomePageState(); 
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(Object context) {
+    
+  }
+  
+}
