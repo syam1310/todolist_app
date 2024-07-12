4f401c64c036ce79337bfca10e17ac63732571de
syam1310 <psyam0175@gmail.com>
2024-07-12 22:08:32 +0530

    Adding ToDoList of version 1 in this have the functanility to add the todo and remove the todo

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> _todos = ['task1', 'task2', 'task3'];
+
+  void _addTodo(){
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        String newTodo = '';
+
+        return AlertDialog(
+          title: Text("Enter New Task Below: "),
+          content: TextField(
+            onChanged: (value){
+              newTodo = value;
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+              child: Text("Cancle")
+            ),
+            TextButton(
+              onPressed: (){
+                setState(() {
+                  _todos.add(newTodo);
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text("Submit")
+            )
+          ],
+        );
+      });
+  } 
+
   @override
-  Widget build(Object context) {
-    
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("To Do List"), backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: ListView.builder(
+        itemCount: _todos.length,
+        itemBuilder: (context, index){
+          final todo = _todos[index];
+
+          return ListTile(
+            title: Text(todo),
+            onTap: () {
+              setState(() {
+                _todos.removeAt(index);
+              });
+            },
+          );
+        }),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _addTodo,
+          ),
+    );
   }
   
 }
