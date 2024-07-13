77f0138a1f06c7dad6340bdac60d1f2976ec8eed
syam1310 <psyam0175@gmail.com>
2024-07-13 09:37:19 +0530

    Adding description along with the title in my todolist app

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,129 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home: MyHomePage(),));  
+class ToDo {
+  final String title;
+  final String description;
+
+  const ToDo(this.title, this.description);
 }
-class MyHomePage extends StatefulWidget {
+
+List<ToDo> toDoList = [
+  ToDo("Paint the house", "Don't forget to use black color"),
+  ToDo("Do Plantation", "Don't forget to put some water"),
+  ToDo("Go to moon", "Don't forget to use rockets")
+];
+
+void main() {
+  runApp(MaterialApp(
+    title: 'App',
+    home: TodosScreen(todos: toDoList),
+  ));
+}
+
+class TodosScreen extends StatefulWidget {
+  final List<ToDo> todos;
+
+  const TodosScreen({Key? key, required this.todos}) : super(key: key);
+
   @override
-  State<MyHomePage> createState() =>  _MyHomePageState(); 
+  State<TodosScreen> createState() => _TodosScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> _todos = ['task1', 'task2', 'task3', "-task5"];
-
-  void _addTodo(){
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        String newTodo = '';
-
-        return AlertDialog(
-          title: Text("Enter New Task Below: "),
-          content: TextField(
-            onChanged: (value){
-              newTodo = value;
-            },
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: (){
-                Navigator.of(context).pop();
+class _TodosScreenState extends State<TodosScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "My Tasks",
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+          itemCount: toDoList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(toDoList[index].title),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            DetailScreen(todo: toDoList[index])));
               },
-              child: Text("Cancle")
-            ),
-            TextButton(
-              onPressed: (){
-                setState(() {
-                  _todos.add(newTodo);
-                });
-                Navigator.of(context).pop();
-              },
-              child: Text("Submit")
-            )
-          ],
-        );
-      });
-  } 
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final ToDo? newToDo = await showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                String? title;
+                String? description;
+
+                return AlertDialog(
+                  title: const Text("Create New Task"),
+                  content: Column(
+                    children: <Widget>[
+                      TextField(
+                        onChanged: (value) {
+                          title = value;
+                        },
+                        decoration: const InputDecoration(labelText: 'Title'),
+                      ),
+                      TextField(
+                        onChanged: (value) {
+                          description = value;
+                        },
+                        decoration:
+                            const InputDecoration(labelText: 'Description'),
+                      ),
+                    ],
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text("Cancle")),
+                    TextButton(
+                        onPressed: () {
+                          if (title != null && description != null) {
+                            Navigator.pop(context, ToDo(title!, description!));
+                          }
+                        },
+                        child: Text("Save"))
+                  ],
+                );
+              });
+          if (newToDo != null) {
+            setState(() {
+              toDoList.add(newToDo);
+            });
+          }
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final ToDo todo;
+
+  const DetailScreen({Key? key, required this.todo}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("To Do List"), backgroundColor: Colors.lightBlueAccent,
+        title: Text(todo.title),
+        backgroundColor: Colors.blue,
       ),
-      body: ListView.builder(
-        itemCount: _todos.length,
-        itemBuilder: (context, index){
-          final todo = _todos[index];
-
-          return ListTile(
-            title: Text(
-              todo,
-              style: TextStyle(
-                decoration: todo.startsWith("-") ? TextDecoration.lineThrough : TextDecoration.none
-              ),
-              ),
-            onTap: () {
-              setState(() {
-                if(todo.startsWith("-")){
-                  _todos[index] = todo.substring(2);
-                }else{
-                  _todos[index] = "- $todo";
-                }
-              });
-              
-            },
-                        
-          );
-        }),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: _addTodo,
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(todo.description),
+      ),
     );
   }
-  
 }
